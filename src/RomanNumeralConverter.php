38f7c99fae0d9c9822366d996a45c14a58acc9a0
Sandeep Seshadri <sseshadri@circlebacklending.com>
2016-07-04 11:50:33 -0400

    First version go roman numeral converter for [1- 4999]

--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -4,5 +4,60 @@ namespace App;
 
 class RomanNumeralConverter
 {
+    /**
+     * Ordered list of roman symbols
+     *
+     * @var array
+     */
+    protected $symbols = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I'
+    ];
 
+    /**
+     * Converts an given integer into roman numeral
+     *
+     * @param $input
+     * @return mixed
+     */
+    public function convert($input)
+    {
+        $this->validateInput($input);
+        $result = [];
+        foreach ($this->symbols as $limit => $symbol) {
+            while ($input >= $limit) {
+                $result[] = $symbol;
+                $input -= $limit;
+            }
+        }
+
+        return implode($result);
+    }
+
+    /**
+     * Checks if the input is valid integer that can be converted to roman numeral
+     *
+     * @param $input
+     */
+    protected function validateInput($input)
+    {
+        if (!is_numeric($input)) {
+            throw new \InvalidArgumentException;
+        }
+
+        if ($input == 0 || $input >=  5000) {
+            throw new \OutOfRangeException;
+        }
+    }
 }
--- tests/RomanNumeralConverterTest.php
+++ tests/RomanNumeralConverterTest.php
@@ -1,15 +1,91 @@
 <?php
 
+use App\RomanNumeralConverter;
 use PHPUnit\Framework\TestCase;
 
 class RomanNumeralConverterTest extends TestCase
 {
+    /**
+     * setup - instantiate the converter to be used in the tests
+     */
+    public function setUp()
+    {
+        $this->converter = new RomanNumeralConverter();
+    }
+
     /** @test */
     public function it_an_instance()
     {
-        $converter = new \App\RomanNumeralConverter();
-        $this->assertInstanceOf(App\RomanNumeralConverter::class, $converter);
+        $this->assertInstanceOf(RomanNumeralConverter::class, $this->converter);
     }
 
-    
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function it_throws_exception_for_invalid_input()
+    {
+        $this->converter->convert('invalid input');
+    }
+
+    /**
+     * @test
+     * @expectedException OutOfRangeException
+     */
+    public function it_throws_exception_for_input_0()
+    {
+        $this->converter->convert(0);
+    }
+
+    /**
+     * @test
+     * @expectedException OutOfRangeException
+     */
+    public function it_throws_exception_for_input_over_4999()
+    {
+        $this->converter->convert(5000);
+    }
+
+    /**
+     * @test
+     * @dataProvider romanNumeralDataProvider
+     * @param $input
+     * @param $expected
+     */
+    public function it_converts_to_roman_numeral($input, $expected)
+    {
+        $result = $this->converter->convert($input);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * list of integers and their roman symbols
+     * @return array
+     */
+    public function romanNumeralDataProvider()
+    {
+        return [
+            [1, 'I'],
+            [2, 'II'],
+            [3, 'III'],
+            [4, 'IV'],
+            [5, 'V'],
+            [6, 'VI'],
+            [7, 'VII'],
+            [9, 'IX'],
+            [10, 'X'],
+            [11, 'XI'],
+            [20, 'XX'],
+            [40, 'XL'],
+            [50, 'L'],
+            [90, 'XC'],
+            [100, 'C'],
+            [400, 'CD'],
+            [500, 'D'],
+            [900, 'CM'],
+            [1000, 'M'],
+            [4000, 'MMMM'],
+            [4999, 'MMMMCMXCIX']
+        ];
+    }
 }
